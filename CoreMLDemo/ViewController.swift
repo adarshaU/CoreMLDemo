3fe3d615cc187bc7aea87b25a4c1ecb4a16f9741
adarshaU <adarsha.upadhya@ecanarys.com>
2018-12-27 14:52:07 +0530

    Initial Commit

---
+++ CoreMLDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreMLDemo
+//
+//  Created by Adarsha Upadhya on 27/12/18.
+//  Copyright © 2018 Adarsha Upadhya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
