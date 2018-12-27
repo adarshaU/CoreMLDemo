ea6fdc769c62fd489b29270e3151b70dd2668f51
adarshaU <adarsha.upadhya@ecanarys.com>
2018-12-27 15:07:42 +0530

    Initial UI commit

--- CoreMLDemo/ViewController.swift
+++ CoreMLDemo/ViewController.swift
@@ -8,13 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
+    @IBOutlet weak var mlImageView: UIImageView!
+    let pickerController:UIImagePickerController = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        pickerController.delegate = self
+        pickerController.sourceType = .camera
+        pickerController.allowsEditing = true
     }
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+    self.present(pickerController, animated: true, completion: nil)
+        
+    }
+    
+}
 
 
+extension ViewController{
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            
+            mlImageView.image = image
+            
+        }
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+    
 }
 
